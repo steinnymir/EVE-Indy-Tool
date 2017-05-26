5e65880341b63a314a086c76979dea90f3ee0af8
steinnymir <sagustss@uni-mainz.de>
2017-05-26 18:13:38 +0200

    pycharm commitment

--- library/indy.py
+++ library/indy.py
@@ -4,14 +4,7 @@ Created on Sat May 20 17:02:12 2017
 
 @author: Stymir
 """
-from openpyxl import load_workbook
-# from . import gfs
-from library import gfs
-import csv
-import os
-import yaml
-import pickle
-
+from library import sde
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
@@ -19,8 +12,8 @@ except ImportError:
 
 
 def main():
-    timer = gfs.Timer()
-    timer.tic()
+    # timer = gfs.Timer()
+    # timer.tic()
 
     # test_bp = Blueprint(28675)
     # test_bp.printName()
@@ -33,8 +26,10 @@ def main():
     #
     # timer.toc()
 
-
-
+    sde = sde.SDE()
+    SDE.import_quick()
+    bp = Blueprint(3529)
+    print(bp.__dict__.keys())
 
 
 
@@ -46,16 +41,52 @@ class EVEItem(object):
         """ initialzie"""
         self.itemID = itemID
         self.itemName = '' # todo: make the id to name interpreter
+        self.sde = SDE  # the full database
+    # from typeIDs.p
+        self.capacity = float
+        self.description = str
+        self.factionID = int
+        self.graphicID = int
+        self.groupID = int
+        self.marketGroupID = int
+        self.mass = int
+        self.masteries = dict
+        self.name = str
+        self.portionSize = int
+        self.published = bool
+        self.raceID = int
+        self.radius = float
+        self.soundID = int
+        self.traits = dict
+        self.volume = float
 
-        self.initialize_attributes(SDE)  # todo: add more variables and corrseponding initializatons
+
+        self.initialize_typeIDs()
 
     def printName(self):
         """ prints ItemID and item name"""
         print(self.itemName + ' has ID: ' + str(self.itemID))
 
-    def initialize_attributes(self, sde):
-        """  """
-        self.itemName = sde.invTypes[self.itemID]['typeName']
+    def initialize_typeIDs(self, sde=None):
+        """ Fetch all data from typeIDs"""
+        if sde is None:
+            sde = self.sde
+        self.capacity = float(sde.typeIDs[self.itemID]['itemName'])
+        self.description = str(sde.typeIDs[self.itemID]['itemName'])
+        self.factionID = int(sde.typeIDs[self.itemID]['itemName'])
+        self.graphicID = int(sde.typeIDs[self.itemID]['itemName'])
+        self.groupID = int(sde.typeIDs[self.itemID]['itemName'])
+        self.marketGroupID = int(sde.typeIDs[self.itemID]['itemName'])
+        self.mass = int(sde.typeIDs[self.itemID]['itemName'])
+        self.masteries = dict(sde.typeIDs[self.itemID]['itemName'])
+        self.name = str(sde.typeIDs[self.itemID]['itemName'])
+        self.portionSize = int(sde.typeIDs[self.itemID]['itemName'])
+        self.published = bool(sde.typeIDs[self.itemID]['itemName'])
+        self.raceID = int(sde.typeIDs[self.itemID]['itemName'])
+        self.radius = float(sde.typeIDs[self.itemID]['itemName'])
+        self.soundID = int(sde.typeIDs[self.itemID]['itemName'])
+        self.traits = dict(sde.typeIDs[self.itemID]['itemName'])
+        self.volume = float(sde.typeIDs[self.itemID]['itemName'])
 
 
 class Blueprint(EVEItem):  # todo: fix import of SDE in EVEitem and Blueprint
@@ -70,6 +101,55 @@ class Blueprint(EVEItem):  # todo: fix import of SDE in EVEitem and Blueprint
         self.maxProductionLimit = 0
         self.product = 0
         self.product_quantity = 0
+        # from blueprints.p
+        self.copying_materials = dict
+        self.copying_skills = dict
+        self.copying_time = int
+        self.invention_materials = dict
+        self.invention_products_probability = float
+        self.invention_products = dict
+        self.manufacturing_materials = dict
+        self.manufacturing_products = dict
+        self.manufacturing_skills = dict
+        self.manufacturing_time = int
+        self.research_material_materials = dict
+        self.research_material_skills = dict
+        self.research_material_time = int
+        self.research_time_materials = dict
+        self.research_time_skills = dict
+        self.research_time_time = int
+        self.blueprintTypeID = int
+        self.maxProductionLimit = int
+
+
+    def initialize_BP(self, sde=None):
+        """ initializes all BP related attributes"""
+        if sde is None:
+            sde = self.sde
+        self.copying_materials = dict
+        self.copying_skills = dict
+        self.copying_time = int
+        self.invention_materials = dict
+        self.invention_products_probability = float
+        self.invention_products = dict
+        self.manufacturing_materials = dict
+        self.manufacturing_products = dict
+        self.manufacturing_skills = dict
+        self.manufacturing_time = int
+        self.research_material_materials = dict
+        self.research_material_skills = dict
+        self.research_material_time = int
+        self.research_time_materials = dict
+        self.research_time_skills = dict
+        self.research_time_time = int
+        self.blueprintTypeID = int
+        self.maxProductionLimit = int
+
+
+
+
+
+
 
     def fetch_bp_data(self):
         """ load info from SDE """
@@ -106,209 +186,6 @@ class Blueprint(EVEItem):  # todo: fix import of SDE in EVEitem and Blueprint
 
 # %% Database Management
 
-class SDE(object):
-    """ Contains a series of dictionaries containing necessary sde data"""
-
-    def __init__(self):
-
-        self.DB_LOCATION_CSV = 'D:/Documents/py_code/EVE_database/csv/'
-        self.DB_LOCATION_YAML = 'D:/Documents/py_code/EVE_database/sde/fsd/'
-        self.DB_LOCATION_PICKLE = 'D:/Documents/py_code/EVE_database/'
-        self.QUICK_IMPORT_LIST = ['invTypes','invCategories','invGroups','invMetaTypes','invMetaGroups','blueprints']
-
-        self.db_list = []
-
-        self.blueprints = None
-        self.invCategories = None
-        self.invGroups = None
-        self.invMetaGroups = None
-        self.invMetaTypes = None
-        self.invTypes = None
-
-
-        self.db_coreIndy = ['invTypes',
-                            'invCategories',
-                            'invGroups',
-                            'invMetaTypes',
-                            'invMetaGroups',
-                            'blueprints']
-
-        self.db_coreIndy_old = ['invTypes',
-                                'invCategories',
-                                'invGroups',
-                                'invMetaTypes',
-                                'invMetaGroups',
-                                'industryActivity',
-                                'industryActivityMaterials',
-                                'industryActivityProbabilities']
-
-        self.activityTypeID = {0: 'None',
-                               1: 'Manufacturing',
-                               2: 'Researching Technology',
-                               3: 'Researching Time Efficiency',
-                               4: 'Researching Material Efficiency',
-                               5: 'Copying',
-                               6: 'Duplicating',
-                               7: 'Reverse Engineering',
-                               8: 'Invention'}
-
-    def import_data(self, dbName):
-        """ imports data from a database file (csv or yaml)"""
-        timer = gfs.Timer()
-        timer.tic()
-        if dbName in ['blueprints', 'typeIDs', 'groupIDs', 'graphicIDs']:
-            print('importing: ' + dbName +  '.yaml')
-            data = self.get_data_yaml(dbName)
-        else:
-            print('importing: ' + dbName + '.csv')
-            data = self.get_data_csv(dbName)
-        setattr(self, dbName, data)
-        dt = timer.toc(out='return')
-        print('Imported {0} in {1:.3f} ms'.format(dbName,dt))
-
-    def export_pickle(self, dbName):
-        """ create a pickle for each database loaded"""
-        filePath = self.DB_LOCATION_PICKLE + dbName + '.p'
-        try:
-            timer = gfs.Timer()
-            timer.tic()
-            data = getattr(self, dbName)
-            with open(filePath, 'wb+') as f:
-                pickle.dump(data, f)
-            dt = timer.toc(out='return')
-            print('Exported {0} in {1:.3f} ms'.format(dbName, dt))
-        except AttributeError:
-            print('no data found with name ' + dbName)
-
-    def export_multiple_pickle(self,dbList):
-        """ import from sde and export to pickle the list of databases given """
-        for db_name in dbList:
-            print('importing: ' + db_name)
-            self.import_data(db_name)
-            print('exporting: ' + db_name)
-            self.export_pickle(db_name)
-
-    def import_pickle(self, dbName):  # todo: untested
-        """ imports data from pickle dumped file"""
-        # try:
-        timer = gfs.Timer()
-        timer.tic()
-        print('Importing: ' + dbName)
-        filePath = self.DB_LOCATION_PICKLE + dbName + '.p'
-        with open(filePath, 'rb') as f:
-            data = pickle.load(f)
-            setattr(self, dbName, data)
-        dt = timer.toc(out='return')
-        print('Imported {0} in {1:.3f} ms'.format(dbName,dt))
-
-    def import_indy_db(self):
-        """ import databases relevant for indy calculator"""
-        for dbName in self.QUICK_IMPORT_LIST:
-            self.import_pickle(dbName)
-
-    def get_filepath(self, db_name, ext):
-        """
-        :return : returns str of database file path.
-        :param db_name: name of database file to load
-        :type ext: str representing db_name file format
-
-        """
-        if ext == 'yaml':
-            return self.DB_LOCATION_CSV + db_name + '.' + ext
-        elif ext == 'csv':
-            return self.DB_LOCATION_YAML + db_name + '.' + ext
-        else:
-            return 'Incorrect file extension.'
-
-    def get_data_yaml(self, db_name):
-        """ imports a YAML database to same name attribute"""
-        with  open(self.DB_LOCATION_YAML + db_name + '.yaml', 'r') as f:
-            data = yaml.load(f)
-        return data
-
-    def get_data_csv(self, db_name):
-        """ imports a CSV database to same name attribute """
-        headline = True
-        data = {}
-        with open(self.DB_LOCATION_CSV + db_name + '.csv', 'r', encoding="utf8") as f:
-            reader = csv.reader(f)
-
-            for line in reader:  # get headers for dict key names
-                if headline:
-                    headers = line
-                    headline = False
-                else:
-                    key = int(line[0])
-                    pos = 0
-
-                    try:  # try appending new values to repeated 1st column items
-                        # otherwise just add new entry
-                        _ = data[key]
-                        for word in line:
-                            label = headers[pos]
-                            pos += 1
-                            val = getNum_or_Str(word)
-                            try:
-                                data[key][label].append(val)
-                            except AttributeError:
-                                tmp = data[key][label]
-                                data[key][label] = [tmp]
-                                data[key][label].append(val)
-
-                    except KeyError:
-                        data[key] = {}
-                        for word in line:
-                            label = headers[pos]
-                            pos += 1
-                            val = gfs.getNum_or_Str(word)
-
-                            data[key][label] = val
-        return (data)
-
-    # --------------- Old and deprectated methods ------------------
-
-    def init_db_list(self):
-        """ list all csv database files"""
-        file_list = os.listdir(self.DB_LOCATION_CSV)
-        for name in file_list:
-            self.db_list.append(name[:-4])
-
-    def import_all_csv(self):
-        """ imports all csv database files"""
-        if len(self.db_list) == 0:
-            self.init_db_list()
-
-        for name in self.db_list:
-            print(name)
-            self.import_data(self.get_filepath(name))
-
-    def import_db_core_indy(self):
-        """ import database files used for industry purposes."""
-        for name in self.db_coreIndy_old:
-            self.import_data(name)
-
-    def open_file_xlsx(self, file):
-        ''' '''
-        wb = load_workbook(file)
-        ws = wb.active
-        self.name = ws['A1'].value
-        # get column headers
-        for col in ws.iter_cols(max_row=1):
-            for cell in col:
-                self.headers.append(cell.value)
-
-        # write to data
-        row_i = 0
-
-        for row in ws.iter_rows(min_row=2):
-
-            row_i += 1
-            key = ws.cell(row=row_i, column=1).value
-            col_i = 0
-            self.data[key] = {}
-            for header in self.headers:
-                col_i += 1
-                self.data[key][header] = ws.cell(row=row_i, column=col_i).value
 
 
 if __name__ == '__main__':
--- master_script.py
+++ master_script.py
@@ -4,36 +4,41 @@ Created on Sat May 20 17:10:26 2017
 
 @author: Steinn Ymir
 """
-from library import indy, gfs, gui
+from library import indy, gfs, gui, sde
 from PyQt5 import QtGui as qg  # (the example applies equally well to PySide)
 from PyQt5 import QtWidgets as qw
 from PyQt5 import QtCore as qc
 import sys
+import os
 
 
 def main():
-    timer = gfs.Timer()
-    timer.tic()
+    # timer = gfs.Timer()
+    # timer.tic()
+    #
+    sde = sde.SDE()
+
+    #
+    # sde.import_quick()
+    #
+    # # sde.import_pickle('blueprints')
+    # print(sde.blueprints[28675])
+    # print(sde.invTypes[28675])
+    # timer.toc_end()
+    #
+    # test_bp = indy.Blueprint(681, sde)
+    # test_bp.printName()
+    # test_bp.fetch_bp_data()
+    # # print(test_bp.activities)
+    # materials = test_bp.get_manufacturing_materials(out='name')
+    # print(materials)
+
+    project_path = os.path.dirname(os.path.realpath(__file__))
+    newpath = os.path.dirname(project_path)
+    newerpath = os.path.dirname(newpath)
+    print(newerpath)
 
 
-
-
-
-    sde = indy.SDE()
-
-    sde.import_indy_db()
-
-     # sde.import_pickle('blueprints')
-    print(sde.blueprints[28675])
-    print(sde.invTypes[28675])
-    timer.toc_end()
-
-    test_bp = indy.Blueprint(681, sde)
-    test_bp.printName()
-    test_bp.fetch_bp_data()
-    #print(test_bp.activities)
-    materials = test_bp.get_manufacturing_materials(out='name')
-    print(materials)
 
 def launchGUI():
     ''' launch the gui '''
@@ -47,5 +52,7 @@ def launchGUI():
 
 
 if __name__ == '__main__':
+
+
     main()
-    #launchGUI()
+    # launchGUI()
